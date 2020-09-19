661ee9253ed90396593259b88dcd559d53ab865d
Hemant Kumar <38708271+HemantTomar@users.noreply.github.com>
2020-09-19 09:11:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
   access_key = var.access_key
   secret_key = var.secret_key
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-01ed306a12b7d1c96"
-  instance_type = "t2.medium"
+  ami           = "ami-0c94855ba95c71c99"
+  instance_type = "t2.micro"
 }
